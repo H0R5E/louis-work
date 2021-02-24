5ca14b6cf95c2e84453299a9607aee81695a00f5
Mathew Topper <damm_horse@yahoo.co.uk>
2021-02-24 10:11:16 +0000

    Check build against espeak 1.50. Works OK.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd build
 conan install ..
 cd ..
 cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release .
-ninja -C build/
+ninja -C build/ -j7 louis
 cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release -DESPEAKNG_BUILD=OFF .
 sudo ninja install -C build/
