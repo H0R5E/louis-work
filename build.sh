e3e59e20e13fe82c591accce3da582d1556cf958
Mathew Topper <damm_horse@yahoo.co.uk>
2021-02-24 09:42:24 +0000

    Imprve installation logic and other options

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ conan install ..
 cd ..
 cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release .
 ninja -C build/
+cmake -GNinja -Bbuild -DCMAKE_BUILD_TYPE=Release -DESPEAKNG_BUILD=OFF .
 sudo ninja install -C build/
