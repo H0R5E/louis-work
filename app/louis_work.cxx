d5d826a80e2c52a49a5f03a5a8e37e44e8211c59
Mathew Topper <damm_horse@yahoo.co.uk>
2024-11-13 13:39:58 +0000

    Use windows entry point to avoid terminal window opening

--- app/louis_work.cxx
+++ app/louis_work.cxx
@@ -16,21 +16,23 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include <signal.h>
-#include <sstream>
+#include "game.h"
+#include "logging.h"
+#include "polymorphic_value.h"
+#include "sound.h"
+#include "window.h"
+
 #include <argparse/argparse.hpp>
 #include <boost/stacktrace.hpp>
 #include <spdlog/spdlog.h>
 
-#include "game.h"
-#include "logging.h"
-#include "sound.h"
-#include "window.h"
-#include "polymorphic_value.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+#include <signal.h>
 
 using namespace isocpp_p0201;
 
@@ -40,79 +42,79 @@ void my_signal_handler(int signum) {
     ::raise(SIGABRT);
 }
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char* argv[]) {
     ::signal(SIGSEGV, &my_signal_handler);
     ::signal(SIGABRT, &my_signal_handler);
-    
+
     argparse::ArgumentParser program("louis");
-    
+
     program.add_argument("-x")
         .help("run using source tree assets")
         .default_value(false)
         .implicit_value(true);
-    
+
     program.add_argument("-v")
         .help("show logging in console")
         .default_value(false)
         .implicit_value(true);
-    
+
     program.add_argument("-V")
         .help("show debug logging in console")
         .default_value(false)
         .implicit_value(true);
-    
+
     try {
         program.parse_args(argc, argv);
-    } catch (const std::runtime_error& err) {
+    } catch (std::runtime_error const& err) {
         std::cout << err.what() << std::endl;
         std::cout << program;
         exit(0);
     }
-    
-    bool test_paths {false};
-    bool console_log {false};
-    bool console_debug {false};
-    
+
+    bool test_paths{false};
+    bool console_log{false};
+    bool console_debug{false};
+
     if (program["-x"] == true) {
         std::cout << "Using local paths" << std::endl;
         test_paths = true;
     }
-    
+
     if (program["-v"] == true) {
         console_log = true;
     }
-    
+
     if (program["-V"] == true) {
         console_log = true;
         console_debug = true;
     }
-    
+
     // Bring up the logger
     setupDefaultLogger(test_paths, console_log, console_debug);
-    
+
     if (std::filesystem::exists("./backtrace.dump")) {
-        
         std::ifstream ifs("./backtrace.dump");
         std::stringstream ss;
-        
+
         boost::stacktrace::stacktrace st =
-                            boost::stacktrace::stacktrace::from_dump(ifs);
+            boost::stacktrace::stacktrace::from_dump(ifs);
         ss << "Previous run crashed:\n" << st << std::endl;
         spdlog::error(ss.str());
-        
+
         ifs.close();
         std::filesystem::remove("./backtrace.dump");
-        
     }
-    
+
     spdlog::info("Starting Louis' Work");
-    
-    Game my_game {std::make_unique<WindowAdapter>(),
-                  make_polymorphic_value<SoundMakerBase, SoundMaker<>>(),
-                  test_paths};
+
+    Game my_game{std::make_unique<WindowAdapter>(),
+                 make_polymorphic_value<SoundMakerBase, SoundMaker<>>(),
+                 test_paths};
     my_game.EventLoop();
-    
+
     return 0;
-    
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd) {
+    return main(__argc, __argv);
 }
--- src/logging.h
+++ src/logging.h
@@ -18,66 +18,64 @@
 
 #pragma once
 
-#include <iostream>
-#include <stdexcept>
-#include <string_view>
+#include "helpers.h"
+#include "pathconfig.h"
+
 #include <sago/platform_folders.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-#include "helpers.h"
-#include "pathconfig.h"
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string_view>
 
-void setupDefaultLogger (const bool& test=false,
-                         const bool& console=false,
-                         const bool& console_debug=false) {
-    
+void setupDefaultLogger(bool const& test = false, bool const& console = false,
+                        bool const& console_debug = false) {
     std::string_view log_dir;
     if (test) {
         log_dir = LOGS_DIR_TEST;
     } else {
         log_dir = LOGS_DIR_RELEASE;
     }
-    
-    auto abs_log_dir = joinPaths(sago::getDataHome(), log_dir).lexically_normal();
-    
-    if (!std::filesystem::is_directory(abs_log_dir) || !std::filesystem::exists(abs_log_dir)) {
+
+    auto abs_log_dir =
+        joinPaths(sago::getDataHome(), log_dir).lexically_normal();
+
+    if (!std::filesystem::is_directory(abs_log_dir) ||
+        !std::filesystem::exists(abs_log_dir)) {
         std::filesystem::create_directories(abs_log_dir);
     }
-    
+
     auto log_path = joinPaths(abs_log_dir, "log.txt");
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-                                            log_path.generic_string(), true);
+        log_path.generic_string(), true);
     file_sink->set_pattern("[%H:%M:%S.%f] [%l] %v");
     file_sink->set_level(spdlog::level::debug);
-    
+
     std::vector<spdlog::sink_ptr> sinks;
     sinks.push_back(file_sink);
-    
+
     if (console) {
-        
-        auto console_sink = std::make_shared<
-                                    spdlog::sinks::stdout_color_sink_mt>();
+        auto console_sink =
+            std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
         console_sink->set_pattern("[%H:%M:%S.%f] [%l] %v");
-        
+
         if (console_debug) {
             console_sink->set_level(spdlog::level::debug);
         } else {
             console_sink->set_level(spdlog::level::info);
         }
-        
+
         sinks.push_back(console_sink);
-        
     }
-        
-    auto combined_logger = std::make_shared<spdlog::logger>("main_logger",
-                                                            begin(sinks),
-                                                            end(sinks));
+
+    auto combined_logger = std::make_shared<spdlog::logger>(
+        "main_logger", begin(sinks), end(sinks));
     combined_logger->set_level(spdlog::level::debug);
     spdlog::set_default_logger(combined_logger);
-    
+
     std::stringstream log_msg;
     log_msg << "Logging to file at: " << log_path.generic_string();
     spdlog::debug(log_msg.str());
-    
 }
